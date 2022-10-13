fdd9c42802d5829c8bf0af8b42f575eea4d9ffd0
unknown <dattuantrinh77@gmail.com>
2022-10-13 22:31:40 +0700

    page checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -12,7 +12,23 @@ export class CheckoutComponent implements OnInit {
   constructor(private cartService:CartService) { }
 
   ngOnInit(): void {
-    this.cartService.getDetailCart(this.cartID).subscribe(res=>this.listItem=res.body)
+    this.displayItemsCart();
   }
-
+  displayItemsCart(){
+    this.cartService.getDetailCart(this.cartID).subscribe(res=>{
+      this.listItem=res.body;
+     /* let sum=0;
+      for (let i=0;i<this.listItemCart.length;i++){
+        sum+=this.listItemCart[i].total_prod;
+      }
+      this.cartService.total$.next(sum);*/
+    })
+  }
+  deleteDetail(item:any){
+    this.cartService.deleteDetail(item.id).subscribe(res=>{
+      if(res.status==202){
+        this.displayItemsCart();
+      }
+    })
+  }
 }
